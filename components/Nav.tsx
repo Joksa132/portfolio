49f52cd18861f2dc9ad64e6868fb49205ea76961
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-28 16:23:12 +0200

    implement smooth scroll

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,36 +4,33 @@ import Link from "next/link";
 type Props = {
   activeLink: string;
   setActiveLink: React.Dispatch<React.SetStateAction<string>>;
+  sectionRefs: { [key: string]: React.RefObject<HTMLElement> }
 }
 
-export default function Nav({ activeLink, setActiveLink }: Props) {
+export default function Nav({ activeLink, setActiveLink, sectionRefs }: Props) {
 
   const handleLinkClick = (link: string) => {
+    const sectionRef = sectionRefs[link]
+    if (sectionRef && sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
     setActiveLink(link)
   }
 
   return (
     <nav className="w-full py-4 shadow-lg flex gap-10 items-center justify-end text-xl sticky top-0">
-      <Link href="#home">
-        <span className={activeLink === "home" ? 'text-blue-500' : ''} onClick={() => handleLinkClick('home')}>
-          Home
-        </span>
-      </Link>
-      <Link href="#about">
-        <span className={activeLink === "about" ? 'text-blue-500' : ''} onClick={() => handleLinkClick('about')}>
-          About
-        </span>
-      </Link>
-      <Link href="#projects">
-        <span className={activeLink === "projects" ? 'text-blue-500' : ''} onClick={() => handleLinkClick('projects')}>
-          Projects
-        </span>
-      </Link>
-      <Link href="#contact">
-        <span className={`${activeLink === 'contact' ? 'text-blue-500' : ''} mr-28`} onClick={() => handleLinkClick('contact')}>
-          Contact
-        </span>
-      </Link>
+      <span className={`${activeLink === 'home' ? 'text-blue-500' : ''} cursor-pointer font-bold`} onClick={() => handleLinkClick('home')}>
+        Home
+      </span>
+      <span className={`${activeLink === 'about' ? 'text-blue-500' : ''} cursor-pointer font-bold`} onClick={() => handleLinkClick('about')}>
+        About
+      </span>
+      <span className={`${activeLink === 'projects' ? 'text-blue-500' : ''} cursor-pointer font-bold`} onClick={() => handleLinkClick('projects')}>
+        Projects
+      </span>
+      <span className={`${activeLink === 'contact' ? 'text-blue-500' : ''} cursor-pointer font-bold mr-28`} onClick={() => handleLinkClick('contact')}>
+        Contact
+      </span>
     </nav>
   )
 }
