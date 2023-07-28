e58fa655ad7e4ecbc0d8d951c989b6c3e58d9ce4
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-28 16:31:08 +0200

    add smooth scroll for button

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 import Nav from '@/components/Nav'
-import Link from 'next/link'
 import { AiOutlineArrowDown } from 'react-icons/ai'
 import { useEffect, useRef, useState } from "react";
 import TechnologyCard from '@/components/TechnologyCard';
 import { technologies } from '@/utils/technologies';
 import emailjs from '@emailjs/browser';
 import { SnackbarProvider, enqueueSnackbar } from 'notistack';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   const [activeLink, setActiveLink] = useState<string>("home")
@@ -75,26 +75,35 @@ export default function Home() {
       });
   }
 
+  const handleHeroButtonClick = () => {
+    const sectionRef = sectionRefs.projects;
+    if (sectionRef && sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+    setActiveLink("projects")
+  }
+
   return (
     <main className='text-white scroll-smooth'>
       <section className="flex flex-col items-center justify-center h-screen text-6xl gap-4" id='home' ref={sectionRefs.home}>
         <span className="text-left-animation">Hello, I'm <span className="text-blue-400">Nikola</span>.</span>
         <span className="text-right-animation mb-6">I'm a web developer.</span>
-        <Link href="#about">
-          <button className="btn-animation rounded-sm text-3xl border-2 border-blue-400 px-6 py-2 text-blue-400 flex items-center gap-4">
-            Check my work <AiOutlineArrowDown />
-          </button>
-        </Link>
+        <button
+          className="btn-animation rounded-sm text-3xl border-2 border-blue-400 px-6 py-2 text-blue-400 flex items-center gap-4"
+          onClick={handleHeroButtonClick}
+        >
+          Check my work <AiOutlineArrowDown />
+        </button>
       </section>
-      <Nav activeLink={activeLink} setActiveLink={setActiveLink} />
+      <Nav activeLink={activeLink} setActiveLink={setActiveLink} sectionRefs={sectionRefs} />
       <section className='h-screen flex flex-col items-center' id='about' ref={sectionRefs.about}>
         <div>
           <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>About</h2>
         </div>
         <div className='mt-20 flex flex-col items-center w-full'>
-          <p className='text-2xl w-2/5'>
+          <p className='text-2xl w-2/4'>
             I am a Bachelor with Honours in Information Technology Engineering, commited to continuous
-            learning with a constant drive to improve my skills.
+            learning with a constant drive to improve my skills in Web Development.
           </p>
           <div className="flex w-2/4 mt-16 flex-col gap-2">
             <div className={`first-row flex gap-2 ${isAboutVisible ? 'card-left-animation' : ''}`}>
@@ -115,47 +124,45 @@ export default function Home() {
           <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>Projects</h2>
         </div>
       </section>
-      <section className='h-screen flex flex-col items-center' id='contact' ref={sectionRefs.contact}>
-        <div>
+      <section className='h-screen flex flex-col justify-between' id='contact' ref={sectionRefs.contact}>
+        <div className='flex flex-col items-center'>
           <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>Contact</h2>
+          <form ref={formRef} onSubmit={handleEmailSubmit} className='flex flex-col items-center gap-2 mt-20 w-full'>
+            <input
+              type="text"
+              name='user_name'
+              id='user_name'
+              placeholder='Name'
+              required
+              className='bg-zinc-800 p-2 w-1/4 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
+            />
+            <input
+              type="email"
+              name="user_email"
+              id="user_email"
+              placeholder='Email'
+              required
+              className='bg-zinc-800 p-2 w-1/4 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
+            />
+            <textarea
+              name='message'
+              id='message'
+              placeholder='Message'
+              required
+              className='bg-zinc-800 p-2 w-1/4 rounded-sm resize-none focus:outline-none focus:ring-1 focus:ring-blue-400'
+              rows={6}
+            />
+            <div className='flex justify-end w-1/4 text-xl font-bold mt-2'>
+              <button
+                type='submit'
+                className='tracking-widest border-b-2 border-blue-400 px-2 hover:border-2 hover:bg-blue-400 hover:text-2xl hover:rounded-sm'
+              >
+                SUBMIT
+              </button>
+            </div>
+          </form>
         </div>
-        <form ref={formRef} onSubmit={handleEmailSubmit} className='flex flex-col items-center gap-2 mt-20 w-full'>
-          <input
-            type="text"
-            name='user_name'
-            id='user_name'
-            placeholder='Name'
-            required
-            className='bg-zinc-800 p-2 w-1/4 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
-          />
-          <input
-            type="email"
-            name="user_email"
-            id="user_email"
-            placeholder='Email'
-            required
-            className='bg-zinc-800 p-2 w-1/4 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
-          />
-          <textarea
-            name='message'
-            id='message'
-            placeholder='Message'
-            required
-            className='bg-zinc-800 p-2 w-1/4 rounded-sm resize-none focus:outline-none focus:ring-1 focus:ring-blue-400'
-            rows={6}
-          />
-          <div className='flex justify-end w-1/4 text-xl font-bold mt-2'>
-            <button
-              type='submit'
-              className='tracking-wider border-b-2 border-blue-400 px-2 hover:border-2 hover:bg-blue-400 hover:text-2xl hover:rounded-sm'
-            >
-              SUBMIT
-            </button>
-          </div>
-        </form>
-        <div className='flex items-center gap-4 mt-12'>
-
-        </div>
+        {/*<Footer />*/}
       </section>
       <SnackbarProvider />
     </main >
