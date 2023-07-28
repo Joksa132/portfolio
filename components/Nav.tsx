28fdd4c5a1800dbe6ffb5f4ec79f0a06b4f29908
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-28 12:38:22 +0200

    change color to blue

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -15,22 +15,22 @@ export default function Nav({ activeLink, setActiveLink }: Props) {
   return (
     <nav className="w-full py-4 shadow-lg flex gap-10 items-center justify-end text-xl sticky top-0">
       <Link href="#home">
-        <span className={activeLink === "home" ? 'text-red-500' : ''} onClick={() => handleLinkClick('home')}>
+        <span className={activeLink === "home" ? 'text-blue-500' : ''} onClick={() => handleLinkClick('home')}>
           Home
         </span>
       </Link>
       <Link href="#about">
-        <span className={activeLink === "about" ? 'text-red-500' : ''} onClick={() => handleLinkClick('about')}>
+        <span className={activeLink === "about" ? 'text-blue-500' : ''} onClick={() => handleLinkClick('about')}>
           About
         </span>
       </Link>
       <Link href="#projects">
-        <span className={activeLink === "projects" ? 'text-red-500' : ''} onClick={() => handleLinkClick('projects')}>
+        <span className={activeLink === "projects" ? 'text-blue-500' : ''} onClick={() => handleLinkClick('projects')}>
           Projects
         </span>
       </Link>
       <Link href="#contact">
-        <span className={`${activeLink === 'contact' ? 'text-red-500' : ''} mr-28`} onClick={() => handleLinkClick('contact')}>
+        <span className={`${activeLink === 'contact' ? 'text-blue-500' : ''} mr-28`} onClick={() => handleLinkClick('contact')}>
           Contact
         </span>
       </Link>
