97c938f179ed20d48c026310ef2a25255f556971
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-28 22:07:20 +0200

    fix styles

--- components/TechnologyCard.tsx
+++ components/TechnologyCard.tsx
@@ -8,9 +8,9 @@ type Props = {
 export default function TechnologyCard({ techName, techImg }: Props) {
 
   return (
-    <div className="p-2 w-36 border-2 rounded-xl border-blue-400 flex flex-col items-center gap-2">
+    <div className="py-5 border rounded-md border-blue-400 flex flex-col items-center gap-2">
       <Image src={techImg} alt={techName + 'image'} width={80} height={60} quality={100} />
-      <span>{techName}</span>
+      <span className="text-sm">{techName}</span>
     </div>
   )
 }
