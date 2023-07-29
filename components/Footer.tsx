28ce109058a9f9915b4649dd45d3ece1f4c7424a
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-29 17:06:18 +0200

    replace cv icon with text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,14 +18,16 @@ export default function Footer({ setActiveLink, sectionRefs }: Props) {
   }
 
   return (
-    <footer className="w-full py-8 shadow-lg flex justify-center gap-20 items-center text-white">
+    <footer className="w-full py-6 shadow-lg flex justify-center gap-10 items-center text-white" style={{ backgroundColor: "#272727" }}>
       <button className="rounded-3xl bg-blue-400 flex items-center justify-center hover:animate-bounce fixed bottom-10 right-10 z-50" onClick={() => handleButtonClick()}>
         <MdKeyboardDoubleArrowUp size={30} />
       </button>
       <Link href={"https://github.com/Joksa132"}>
         <Image src={"/github-mark-white.svg"} alt="GitHub profile" width={40} height={40} quality={100} />
       </Link>
-      <Image src={"/resume-icon.svg"} alt="Resume Link" width={50} height={50} quality={100} />
+      <Link href={"/"}>
+        <span className="text-4xl cursor-pointer select-none">CV</span>
+      </Link>
     </footer>
   )
 }
