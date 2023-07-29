eb5024d5dbafa02b91ca87261e78fb1b8f37816e
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-29 22:04:39 +0200

    remove not used prop

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
+
 import Nav from '@/components/Nav'
 import { useEffect, useRef, useState } from "react";
-
 import HomeSection from '@/components/HomeSection';
 import AboutSection from '@/components/AboutSection';
 import ProjectsSection from '@/components/ProjectsSection';
@@ -17,7 +17,6 @@ export default function Home() {
     projects: useRef<HTMLElement>(null),
     contact: useRef<HTMLElement>(null),
   };
-
 
   useEffect(() => {
     const sectionObserver = new IntersectionObserver(
@@ -62,12 +61,10 @@ export default function Home() {
     };
   }, [sectionRefs, isAboutVisible]);
 
-
-
   return (
     <main className='text-white scroll-smooth'>
       <Nav activeLink={activeLink} setActiveLink={setActiveLink} sectionRefs={sectionRefs} showNavBackground={showNavBackground} />
-      <HomeSection activeLink={activeLink} setActiveLink={setActiveLink} sectionRefs={sectionRefs} />
+      <HomeSection setActiveLink={setActiveLink} sectionRefs={sectionRefs} />
       <AboutSection isAboutVisible={isAboutVisible} sectionRefs={sectionRefs} />
       <ProjectsSection sectionRefs={sectionRefs} />
       <ContactSection setActiveLink={setActiveLink} sectionRefs={sectionRefs} />
