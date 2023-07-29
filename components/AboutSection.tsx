39da91868e0729cea2d5b402ae19dd2d6bdfd80f
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-29 21:58:35 +0200

    move about section into separate component

---
+++ components/AboutSection.tsx
@@ -0,0 +1,38 @@
+import TechnologyCard from '@/components/TechnologyCard';
+import { technologies } from '@/utils/technologies';
+
+type Props = {
+  isAboutVisible: boolean;
+  sectionRefs: { [key: string]: React.RefObject<HTMLElement> };
+}
+
+export default function AboutSection({ sectionRefs, isAboutVisible }: Props) {
+  const firstFiveTechnologies = technologies.slice(0, 5);
+  const secondFiveTechnologies = technologies.slice(5, 10);
+
+  return (
+    <section className='h-screen flex flex-col items-center' id='about' ref={sectionRefs.about}>
+      <div className='w-2/4 flex'>
+        <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>About</h2>
+      </div>
+      <div className='mt-5 flex flex-col items-center w-full'>
+        <p className='text-lg w-2/4'>
+          I am a Bachelor with Honours in Information Technology Engineering, commited to continuous
+          learning with a constant drive to improve my skills in Web Development.
+        </p>
+        <div className="w-2/4 mt-10 grid gap-2">
+          <div className={`first-row grid grid-cols-5 gap-2 opacity-0 ${isAboutVisible ? 'card-left-animation' : ''}`}>
+            {firstFiveTechnologies.map((technology, index) => (
+              <TechnologyCard key={index} techName={technology?.name} techImg={technology?.image} />
+            ))}
+          </div>
+          <div className={`second-row grid grid-cols-5 gap-2 opacity-0 ${isAboutVisible ? 'card-right-animation' : ''}`}>
+            {secondFiveTechnologies.map((technology, index) => (
+              <TechnologyCard key={index} techName={technology?.name} techImg={technology?.image} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
