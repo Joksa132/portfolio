dae5cd9d7c8f52b7f442ae3ca618a4eb8d15ed3c
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-30 12:21:03 +0200

    fix height and width

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -11,16 +11,16 @@ export default function AboutSection({ sectionRefs, isAboutVisible }: Props) {
   const secondFiveTechnologies = technologies.slice(5, 10);
 
   return (
-    <section className='h-screen flex flex-col items-center' id='about' ref={sectionRefs.about}>
-      <div className='w-2/4 flex'>
+    <section className='min-h-screen overflow-y-auto flex flex-col items-center' id='about' ref={sectionRefs.about}>
+      <div className='w-4/6 flex'>
         <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>About</h2>
       </div>
       <div className='mt-5 flex flex-col items-center w-full'>
-        <p className='text-lg w-2/4'>
+        <p className='text-lg w-4/6'>
           I am a Bachelor with Honours in Information Technology Engineering, commited to continuous
           learning with a constant drive to improve my skills in Web Development.
         </p>
-        <div className="w-2/4 mt-10 grid gap-2">
+        <div className="w-4/6 mt-10 grid gap-2">
           <div className={`first-row grid grid-cols-5 gap-2 opacity-0 ${isAboutVisible ? 'card-left-animation' : ''}`}>
             {firstFiveTechnologies.map((technology, index) => (
               <TechnologyCard key={index} techName={technology?.name} techImg={technology?.image} />
