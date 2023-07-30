0186108ca63316d198fc55420526d160a72445a9
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-30 12:20:43 +0200

    create projects array

---
+++ utils/projects.ts
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    name: "To-do List",
+    image: "/test-image.jpg",
+    description: "To-do list app built using React, Next.js, TypeScript, Prisma and PostgreSQL",
+    liveLink: "https://todo-list-joksa132.vercel.app",
+    gitHubLink: {
+      frontend: "https://github.com/Joksa132/todo-list",
+    },
+    right: true,
+  },
+  {
+    name: "Ecommerce",
+    image: "/test-image.jpg",
+    description: "Ecommerce website built using React, Next.js, Prisma and PostgreSQL",
+    liveLink: "https://ecommerce-joksa132.vercel.app",
+    gitHubLink: {
+      frontend: "https://github.com/Joksa132/ecommerce",
+    },
+    right: false,
+  },
+  {
+    name: "Odinbook",
+    image: "/test-image.jpg",
+    description: "Social media website built using React, Express, Mongoose and MongoDB",
+    liveLink: "",
+    gitHubLink: {
+      frontend: "https://github.com/Joksa132/odinbook-frontend",
+      backend: "https://github.com/Joksa132/odinbook-backend",
+    },
+    right: true,
+  },
+  {
+    name: "Blog",
+    image: "/test-image.jpg",
+    description: "Blog website built using React, Express, Mongoose and MongoDB",
+    liveLink: "",
+    gitHubLink: {
+      frontend: "https://github.com/Joksa132/blog-frontend",
+      backend: "https://github.com/Joksa132/blog-backend",
+    },
+    right: false,
+  },
+]
