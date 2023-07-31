c7a2575441b2067e11dd2822bdeb2ea4e5b26690
Joksa132 <nikolajoksimovic419@gmail.com>
2023-07-31 16:51:29 +0200

    remove gap

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function ProjectsSection({ sectionRefs }: Props) {
 
   return (
-    <section className='min-h-screen overflow-y-auto flex flex-col items-center gap-4' id='projects' ref={sectionRefs.projects}>
+    <section className='min-h-screen overflow-y-auto flex flex-col items-center' id='projects' ref={sectionRefs.projects}>
       <div className='flex w-4/6'>
         <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>Projects</h2>
       </div>
