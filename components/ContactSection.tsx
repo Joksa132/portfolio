fe72886a8577300ef1203ecb86209b5905d1f230
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-01 15:55:30 +0200

    add labels for inputs and fix positioning

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -40,38 +40,44 @@ export default function ContactSection({ sectionRefs, setActiveLink }: Props) {
     <section className='min-h-screen overflow-y-auto flex flex-col justify-between' id='contact' ref={sectionRefs.contact}>
       <div className='flex flex-col items-center'>
         <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>Contact</h2>
-        <form ref={formRef} onSubmit={handleEmailSubmit} className='flex flex-col items-center gap-2 mt-20 w-full'>
-          <input
-            type="text"
-            name='user_name'
-            id='user_name'
-            placeholder='Name'
-            required
-            className='bg-zinc-800 p-2 w-1/4 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-            value={formData.name}
-          />
-          <input
-            type="email"
-            name="user_email"
-            id="user_email"
-            placeholder='Email'
-            required
-            className='bg-zinc-800 p-2 w-1/4 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
-            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-            value={formData.email}
-          />
-          <textarea
-            name='message'
-            id='message'
-            placeholder='Message'
-            required
-            className='bg-zinc-800 p-2 w-1/4 rounded-sm resize-none focus:outline-none focus:ring-1 focus:ring-blue-400'
-            rows={6}
-            onChange={(e) => setFormData({ ...formData, message: e.target.value })}
-            value={formData.message}
-          />
-          <div className='flex justify-end w-1/4 text-xl font-bold mt-2'>
+        <form ref={formRef} onSubmit={handleEmailSubmit} className='flex flex-col items-center gap-2 mt-20 w-1/4'>
+          <div className="flex flex-col w-full gap-1">
+            <label htmlFor="user_name">Name *</label>
+            <input
+              type="text"
+              name='user_name'
+              id='user_name'
+              required
+              className='bg-zinc-800 p-2 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
+              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+              value={formData.name}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-1">
+            <label htmlFor="user_email">Email *</label>
+            <input
+              type="email"
+              name="user_email"
+              id="user_email"
+              required
+              className='bg-zinc-800 p-2 rounded-sm focus:outline-none focus:ring-1 focus:ring-blue-400'
+              onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+              value={formData.email}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-1">
+            <label htmlFor="message">Message *</label>
+            <textarea
+              name='message'
+              id='message'
+              required
+              className='bg-zinc-800 p-2 rounded-sm resize-none focus:outline-none focus:ring-1 focus:ring-blue-400'
+              rows={6}
+              onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+              value={formData.message}
+            />
+          </div>
+          <div className='flex justify-end w-full text-xl font-bold mt-2'>
             <button
               type='submit'
               className="tracking-widest border-b-2 border-blue-400 px-2 py-1
