029e8a5583f2d7b85cf478f2411f8ddd35ffa863
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-02 20:25:22 +0200

    change img size and fix html validator button err

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -35,9 +35,9 @@ export default function ProjectCard({ project }: Props) {
             width={1920}
             height={1080}
             quality={100}
-            sizes="40vw"
+            sizes="50vw"
             onClick={() => setShowImageModal(true)}
-            style={{ cursor: "pointer" }}
+            style={{ cursor: "pointer", height: "auto", width: "700px" }}
           />
           {showImageModal && (
             <ImageModal image={project.image} setShowImageModal={setShowImageModal} />
@@ -46,25 +46,27 @@ export default function ProjectCard({ project }: Props) {
             <h2 className="text-4xl font-bold mb-6">{project.name}</h2>
             <p className="text-lg">{project.description}</p>
             <div className="flex items-center gap-4 mt-6">
-              <Link href={project.liveLink} target="_blank">
-                <button className="border-2 p-2 font-bold border-blue-400 rounded-sm text-blue-400
+              <Link
+                href={project.liveLink}
+                target="_blank"
+                className="border-2 p-2 font-bold border-blue-400 rounded-sm text-blue-400
                 relative ease-in duration-300 z-10
                 before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
                 after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
                 before:top-0 before:bottom-0 after:top-0 after:bottom-0
                 before:right-full before:left-0 after:right-0 after:left-full
-                hover:before:right-1/2 hover:after:left-1/2 hover:text-white">
-                  Live preview
-                </button>
+                hover:before:right-1/2 hover:after:left-1/2 hover:text-white"
+              >
+                Live preview
               </Link>
               <div>
                 {project.gitHubLink.backend ?
                   <>
                     <button className="font-bold p-1 border-b-2 border-blue-400
-                  relative ease-in duration-300 z-10
-                before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
-                after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
-                  before:bottom-0 before:top-full before:right-0 before:left-0 hover:before:top-0"
+                      relative ease-in duration-300 z-10
+                    before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
+                    after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
+                      before:bottom-0 before:top-full before:right-0 before:left-0 hover:before:top-0"
                       onClick={() => setShowModal(true)}
                     >
                       Github Repo
@@ -74,15 +76,16 @@ export default function ProjectCard({ project }: Props) {
                     )}
                   </>
                   :
-                  <Link href={project.gitHubLink.frontend} target="_blank">
-                    <button className="font-bold p-1 border-b-2 border-blue-400
+                  <Link
+                    href={project.gitHubLink.frontend}
+                    target="_blank"
+                    className="font-bold p-1 border-b-2 border-blue-400
                     relative ease-in duration-300 z-10
                   before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
                   after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
                     before:bottom-0 before:top-full before:right-0 before:left-0 hover:before:top-0"
-                    >
-                      Github Repo
-                    </button>
+                  >
+                    Github Repo
                   </Link>
                 }
               </div>
@@ -94,25 +97,27 @@ export default function ProjectCard({ project }: Props) {
             <h2 className="text-4xl font-bold mb-6">{project.name}</h2>
             <p className="text-lg">{project.description}</p>
             <div className="flex items-center gap-4 mt-6">
-              <Link href={project.liveLink} target="_blank">
-                <button className="border-2 p-2 font-bold border-blue-400 rounded-sm text-blue-400
-              relative ease-in duration-300 z-10
-              before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
-              after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
-              before:top-0 before:bottom-0 after:top-0 after:bottom-0
-              before:right-full before:left-0 after:right-0 after:left-full
-              hover:before:right-1/2 hover:after:left-1/2 hover:text-white">
-                  Live preview
-                </button>
+              <Link
+                href={project.liveLink}
+                target="_blank"
+                className="border-2 p-2 font-bold border-blue-400 rounded-sm text-blue-400
+                relative ease-in duration-300 z-10
+                before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
+                after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
+                 before:top-0 before:bottom-0 after:top-0 after:bottom-0
+                 before:right-full before:left-0 after:right-0 after:left-full
+                 hover:before:right-1/2 hover:after:left-1/2 hover:text-white"
+              >
+                Live preview
               </Link>
               <div>
                 {project.gitHubLink.backend ?
                   <>
                     <button className="font-bold p-1 border-b-2 border-blue-400
-                relative ease-in duration-300 z-10
-              before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
-              after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
-                before:bottom-0 before:top-full before:right-0 before:left-0 hover:before:top-0"
+                    relative ease-in duration-300 z-10
+                  before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
+                  after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
+                    before:bottom-0 before:top-full before:right-0 before:left-0 hover:before:top-0"
                       onClick={() => setShowModal(true)}
                     >
                       Github Repo
@@ -122,15 +127,16 @@ export default function ProjectCard({ project }: Props) {
                     )}
                   </>
                   :
-                  <Link href={project.gitHubLink.frontend} target="_blank">
-                    <button className="font-bold p-1 border-b-2 border-blue-400
-                  relative ease-in duration-300 z-10
-                before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
-                after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
-                  before:bottom-0 before:top-full before:right-0 before:left-0 hover:before:top-0"
-                    >
-                      Github Repo
-                    </button>
+                  <Link
+                    href={project.gitHubLink.frontend}
+                    target="_blank"
+                    className="font-bold p-1 border-b-2 border-blue-400
+                    relative ease-in duration-300 z-10
+                  before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
+                  after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
+                    before:bottom-0 before:top-full before:right-0 before:left-0 hover:before:top-0"
+                  >
+                    Github Repo
                   </Link>
                 }
               </div>
@@ -142,9 +148,9 @@ export default function ProjectCard({ project }: Props) {
             width={1920}
             height={1080}
             quality={100}
-            sizes="40vw"
+            sizes="50vw"
             onClick={() => setShowImageModal(true)}
-            style={{ cursor: "pointer" }}
+            style={{ cursor: "pointer", height: "auto", width: "700px" }}
           />
           {showImageModal && (
             <ImageModal image={project.image} setShowImageModal={setShowImageModal} />
