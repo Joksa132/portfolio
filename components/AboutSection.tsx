46c04414e3271cc9ccd7ab72f2266ee3637f77f9
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-03 19:42:46 +0200

    make about section responsive

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -13,20 +13,24 @@ export default function AboutSection({ sectionRefs, isAboutVisible }: Props) {
   return (
     <section className='flex flex-col items-center' id='about' ref={sectionRefs.about}>
       <div className='w-4/6 flex'>
-        <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>About</h2>
+        <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold 
+        max-md:text-4xl max-md:border-b-4 max-[500px]:text-3xl max-[400px]:text-2xl'
+        >
+          About
+        </h2>
       </div>
       <div className='mt-5 flex flex-col items-center w-full'>
-        <p className='text-lg w-4/6'>
+        <p className='text-lg w-4/6 max-md:text-sm max-[400px]:text-xs'>
           I am a Bachelor with Honours in Information Technology Engineering, commited to continuous
           learning with a constant drive to improve my skills in Web Development.
         </p>
         <div className="w-4/6 mt-10 grid gap-2">
-          <div className={`first-row grid grid-cols-5 gap-2 opacity-0 ${isAboutVisible ? 'card-left-animation' : ''}`}>
+          <div className={`first-row grid grid-cols-5 gap-2 opacity-0 max-sm:grid-cols-3 max-[450px]:grid-cols-2 ${isAboutVisible ? 'card-left-animation' : ''}`}>
             {firstFiveTechnologies.map((technology, index) => (
               <TechnologyCard key={index} techName={technology?.name} techImg={technology?.image} />
             ))}
           </div>
-          <div className={`second-row grid grid-cols-5 gap-2 opacity-0 ${isAboutVisible ? 'card-right-animation' : ''}`}>
+          <div className={`second-row grid grid-cols-5 gap-2 opacity-0 max-sm:grid-cols-3 max-[450px]:grid-cols-2 ${isAboutVisible ? 'card-right-animation' : ''}`}>
             {secondFiveTechnologies.map((technology, index) => (
               <TechnologyCard key={index} techName={technology?.name} techImg={technology?.image} />
             ))}
