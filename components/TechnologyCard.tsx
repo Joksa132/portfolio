8896e1763fed09edc0c028262900e5804908bb6d
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-03 19:42:53 +0200

    make cards responsive

--- components/TechnologyCard.tsx
+++ components/TechnologyCard.tsx
@@ -8,9 +8,15 @@ type Props = {
 export default function TechnologyCard({ techName, techImg }: Props) {
 
   return (
-    <div className="py-5 border rounded-md border-blue-400 flex flex-col items-center gap-2">
-      <Image src={techImg} alt={techName + 'image'} width={80} height={60} quality={100} />
-      <span className="text-sm">{techName}</span>
+    <div className="py-4 border rounded-md border-blue-400 flex flex-col items-center gap-2">
+      <img
+        src={techImg}
+        alt={techName + 'image'}
+        width={80}
+        height={60}
+        className="max-md:w-16"
+      />
+      <span className="text-sm max-md:text-xs">{techName}</span>
     </div>
   )
 }
