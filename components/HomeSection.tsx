57c3324ca8035ad628bf73f79221f8a070ddb1e8
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-06 18:17:50 +0200

    fix class formatting

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -17,24 +17,15 @@ export default function HomeSection({ setActiveLink, sectionRefs }: Props) {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-6xl gap-4 
-    max-[900px]:text-4xl max-sm:text-4xl max-[500px]:text-3xl max-[390px]:text-2xl"
+    <div className="flex flex-col items-center justify-center h-screen text-6xl gap-4 max-[900px]:text-4xl max-sm:text-4xl max-[500px]:text-3xl max-[390px]:text-2xl"
       id='home'
       ref={sectionRefs.home}
     >
       <span className="text-left-animation">Hello, I'm <span className="text-blue-400 font-bold">Nikola</span>.</span>
       <span className="text-right-animation mb-6 max-[400px]:mb-2">I'm a web developer.</span>
       <button
-        className="btn-animation rounded-sm text-3xl border-2 border-blue-400 px-6 py-2 text-blue-400 flex items-center gap-4
-          relative ease-in duration-500 z-1 font-bold
-          before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10
-          after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10
-          before:top-0 before:bottom-0 after:top-0 after:bottom-0
-          before:right-full before:left-0
-          after:right-0 after:left-full
-          hover:before:right-1/2 hover:after:left-1/2 hover:text-white
-          max-[900px]:text-2xl
-          max-[500px]:text-xl max-[400px]:text-sm"
+        className="btn-animation rounded-sm text-3xl border-2 border-blue-400 px-6 py-2 text-blue-400 flex items-center gap-4 relative ease-in duration-500 z-1 font-bold before:bg-blue-400 before:ease-in before:duration-300 before:absolute before:-z-10 after:bg-blue-400 after:ease-in after:duration-300 after:absolute after:-z-10 before:top-0 before:bottom-0 after:top-0 after:bottom-0
+          before:right-full before:left-0 after:right-0 after:left-full hover:before:right-1/2 hover:after:left-1/2 hover:text-white max-[900px]:text-2xl max-[500px]:text-xl max-[400px]:text-sm"
         onClick={handleHeroButtonClick}
       >
         Check my work <AiOutlineArrowDown />
