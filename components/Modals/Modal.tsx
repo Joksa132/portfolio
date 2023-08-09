b83869961fd27e127abae1bba635f5e7f8502e04
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-09 16:53:07 +0200

    make modal responsive

--- components/Modals/Modal.tsx
+++ components/Modals/Modal.tsx
@@ -24,9 +24,9 @@ export default function Modal({ setShowModal, project }: Props) {
   return (
     <>
       <div className="justify-center items-center flex fixed inset-0 z-50">
-        <div className="rounded-lg relative flex flex-col bg-[#272727] p-2 w-80">
+        <div className="rounded-lg relative flex flex-col bg-[#272727] p-2 w-80 max-[500px]:w-60 max-[350px]:w-48">
           <div className="flex justify-between p-2">
-            <h3 className="text-3xl font-bold">
+            <h3 className="text-3xl font-bold max-[500px]:text-2xl">
               Open:
             </h3>
             <button className="p-1 text-2xl hover:text-blue-400" onClick={() => setShowModal(false)}>
@@ -36,7 +36,7 @@ export default function Modal({ setShowModal, project }: Props) {
           <div className="relative p-2 flex flex-col">
             <Link href={project.gitHubLink.frontend} target="_blank">
               <span
-                className="p-1 rounded-md text-lg text-white hover:bg-blue-500 flex items-center gap-1"
+                className="p-1 rounded-md text-lg text-white hover:bg-blue-500 flex items-center gap-1 max-[500px]:text-[15px]"
                 onClick={() => setShowModal(false)}
               >
                 <HiOutlineExternalLink />
@@ -46,7 +46,7 @@ export default function Modal({ setShowModal, project }: Props) {
             {project.gitHubLink.backend && (
               <Link href={project.gitHubLink.backend} target="_blank">
                 <span
-                  className="p-1 rounded-md text-lg text-white hover:bg-blue-500 flex items-center gap-1"
+                  className="p-1 rounded-md text-lg text-white hover:bg-blue-500 flex items-center gap-1 max-[500px]:text-[15px]"
                   onClick={() => setShowModal(false)}
                 >
                   <HiOutlineExternalLink />
