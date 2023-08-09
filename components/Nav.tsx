191d103104e93d86cc56c9015b81e7f5dd521415
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-09 16:05:29 +0200

    fix nav responsiveness

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -19,28 +19,28 @@ export default function Nav({ activeLink, setActiveLink, sectionRefs, showNavBac
 
   return (
     <nav
-      className={`w-full py-4 px-2 flex gap-5 items-center justify-end text-xl sticky top-0 z-30 max-md:text-lg max-[600px]:text-sm max-[600px]:gap-2 max-[500px]:text-xs ${showNavBackground ? 'shadow-lg' : ''}`} style={showNavBackground ? { backgroundColor: "#272727" } : {}}
+      className={`w-full py-4 flex gap-5 items-center justify-end text-xl sticky top-0 z-30 max-[600px]:text-sm max-[600px]:gap-1 max-[400px]:py-2 max-[350px]:text-[12px] ${showNavBackground ? 'shadow-lg' : ''}`} style={showNavBackground ? { backgroundColor: "#272727" } : {}}
     >
       <span
-        className={`${activeLink === 'home' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 max-[400px]:px-0 before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white`}
+        className={`${activeLink === 'home' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white`}
         onClick={() => handleLinkClick('home')}
       >
         Home
       </span>
       <span
-        className={`${activeLink === 'about' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 max before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white`}
+        className={`${activeLink === 'about' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white`}
         onClick={() => handleLinkClick('about')}
       >
         About
       </span>
       <span
-        className={`${activeLink === 'projects' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 max-[400px]:px-0 before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white`}
+        className={`${activeLink === 'projects' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white`}
         onClick={() => handleLinkClick('projects')}
       >
         Projects
       </span>
       <span
-        className={`${activeLink === 'contact' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 max-[400px]:px-0 before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white`}
+        className={`${activeLink === 'contact' ? 'text-blue-400' : ''} cursor-pointer font-bold relative ease-in duration-300 z-10 px-2 before:bg-blue-400 before:ease-in before:duration-300 before:-z-10 before:absolute after:bg-blue-400 after:ease-in after:duration-300 after:-z-10 after:absolute before:top-0 before:right-full before:bottom-0 before:left-0 hover:before:right-0 hover:text-white mr-8`}
         onClick={() => handleLinkClick('contact')}
       >
         Contact
