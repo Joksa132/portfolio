f5c73a30c51b7cc2ab74b6b022a50a671ff2c5cc
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-09 16:26:04 +0200

    fix project section responsiveness

--- components/Sections/ProjectsSection.tsx
+++ components/Sections/ProjectsSection.tsx
@@ -9,8 +9,8 @@ export default function ProjectsSection({ sectionRefs }: Props) {
 
   return (
     <section className='flex flex-col items-center' id='projects' ref={sectionRefs.projects}>
-      <div className='flex w-4/6'>
-        <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold'>Projects</h2>
+      <div className='flex w-4/6 max-xl:w-5/6 max-lg:w-11/12'>
+        <h2 className='text-6xl mt-20 border-b-8 border-blue-400 font-bold max-[550px]:text-5xl'>Projects</h2>
       </div>
       {projects.map((project, index) => (
         <ProjectCard key={index} project={project} />
