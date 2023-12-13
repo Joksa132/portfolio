84373e83eae1c1d594fd1daa986413cb298e5c20
Joksa132 <nikolajoksimovic419@gmail.com>
2023-12-13 16:48:35 +0100

    add 2 new projects

--- utils/projects.ts
+++ utils/projects.ts
@@ -1,8 +1,31 @@
 export const projects = [
   {
+    name: "Link-Sharing App",
+    image: "/projects/link-sharing-app.png",
+    description:
+      "Link-sharing app built using React, Next.js, TypeScript and PostgreSQL",
+    liveLink: "https://link-sharing-app-joksa132.vercel.app/",
+    gitHubLink: {
+      frontend: "https://github.com/Joksa132/link-sharing-app",
+    },
+    right: true,
+  },
+  {
+    name: "Markdown Playground",
+    image: "/projects/markdown-playground.png",
+    description:
+      "Markdown playground app built using React, Next.js and TypeScript",
+    liveLink: "https://markdown-playground-joksa132.vercel.app/",
+    gitHubLink: {
+      frontend: "https://github.com/Joksa132/markdown-playground",
+    },
+    right: false,
+  },
+  {
     name: "To-do List",
     image: "/projects/to-do-list.png",
-    description: "To-do list app built using React, Next.js, TypeScript, Prisma and PostgreSQL",
+    description:
+      "To-do list app built using React, Next.js, TypeScript, Prisma and PostgreSQL",
     liveLink: "https://todo-list-joksa132.vercel.app",
     gitHubLink: {
       frontend: "https://github.com/Joksa132/todo-list",
@@ -12,7 +35,8 @@ export const projects = [
   {
     name: "Ecommerce",
     image: "/projects/ecommerce.png",
-    description: "Ecommerce website built using React, Next.js, Prisma and PostgreSQL",
+    description:
+      "Ecommerce website built using React, Next.js, Prisma and PostgreSQL",
     liveLink: "https://ecommerce-joksa132.vercel.app",
     gitHubLink: {
       frontend: "https://github.com/Joksa132/ecommerce",
@@ -22,7 +46,8 @@ export const projects = [
   {
     name: "Odinbook",
     image: "/projects/odinbook.png",
-    description: "Social media website built using React, Express, Mongoose and MongoDB",
+    description:
+      "Social media website built using React, Express, Mongoose and MongoDB",
     liveLink: "",
     gitHubLink: {
       frontend: "https://github.com/Joksa132/odinbook-frontend",
@@ -33,7 +58,8 @@ export const projects = [
   {
     name: "Blog",
     image: "/projects/blog.png",
-    description: "Blog website built using React, Express, Mongoose and MongoDB",
+    description:
+      "Blog website built using React, Express, Mongoose and MongoDB",
     liveLink: "",
     gitHubLink: {
       frontend: "https://github.com/Joksa132/blog-frontend",
@@ -41,4 +67,4 @@ export const projects = [
     },
     right: false,
   },
-]
+];
